737a3e102774ae395bd136f2abf61992900f0cf9
yamyam030 <87645195+yamyam030@users.noreply.github.com>
2021-07-27 16:07:32 +0900

    1065번: 한수

---
+++ 0727_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+	int a[4], i, n, count = 0;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		int temp = i, lastIndex = 0;
+		while (temp != 0)
+		{
+			a[lastIndex++] = (temp % 10);
+			temp /= 10;
+		}
+		int gap = a[0] - a[1], j = 1, rslt = 1;
+		while ((j + 1) < lastIndex)
+		{
+			if ((a[j] - a[j + 1]) != gap) { rslt = -1; break; }
+			j++;
+		}
+		if (rslt == 1)count++;
+	}
+	printf("%d", count);
+}
